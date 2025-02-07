29d41b851586b5c59df0f09727e8efac74d80ba1
Jiahan Zhang <jzhang@tenstorrent.com>
2025-02-07 10:29:45 -0600

    Add support for G-stage pages to be marked dirty speculatively in service of VS-stage nonleaf PTE translations.

--- Hart.hpp
+++ Hart.hpp
@@ -409,6 +409,11 @@ namespace WdRiscv
     void enableHintOps(bool flag)
     { hintOps_ = flag; }
 
+    /// Enable speculatively marking G-stage page tables dirty for non-leaf
+    /// PTEs.
+    void enableDirtyGForVsNonleaf(bool flag)
+    { virtMem_.enableDirtyGForVsNonleaf(flag); }
+
     /// Enable page based memory types.
     void enableTranslationPbmt(bool flag)
     { enableExtension(RvExtension::Svpbmt, flag); updateTranslationPbmt(); }
