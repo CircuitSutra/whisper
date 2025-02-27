CircuitSutra/whisper

commit 3e88a8b9cdaf496f68466c943fbde8256d8b65c4

Make Uart8250::monitorStding work when the standard input is not a terminal.

Uart8250.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <unistd.h>
 #include <poll.h>
-#include <termios.h>
 #include "Uart8250.hpp"
 
 
@@ -15,11 +14,17 @@ Uart8250::Uart8250(uint64_t addr, uint64_t size)
   auto func = [this]() { this->monitorStdin(); };
   stdinThread_ = std::thread(func);
 
-  struct termios term;
-  tcgetattr(fileno(stdin), &term);
-  cfmakeraw(&term);
-  term.c_lflag &= ~ECHO;
-  tcsetattr(fileno(stdin), 0, &term);
+  int fd = fileno(stdin);        // stdin file descriptor
+  if (isatty(fd))
+    {
+      struct termios term;
+      tcgetattr(fd, &term);
+      termAttr_ = term;  // Save terminal state to restore in destructor.
+
+      cfmakeraw(&term);          // Make terminal raw.
+      term.c_lflag &= ~ECHO;     // Turn off echo.
+      tcsetattr(fd, 0, &term);
+    }
 }
 
 
@@ -27,6 +32,11 @@ Uart8250::~Uart8250()
 {
   terminate_ = true;
   stdinThread_.join();
+
+  int fd = fileno(stdin);        // stdin file descriptor
+
+  if (isatty(fd))
+    tcsetattr(fd, 0, &termAttr_);  // Restore terminal state.
 }
 
 
@@ -131,9 +141,22 @@ Uart8250::write(uint64_t addr, uint32_t value)
 void
 Uart8250::monitorStdin()
 {
+  int fd = fileno(stdin);        // stdin file descriptor
+  if (not isatty(fd))
+    {
+      char c = 0;
+      while (::read(fd, &c, sizeof(c) == 1))
+        {
+          rx_fifo.push(c);
+
+          while (rx_fifo.size() > 1024)
+            ;  // Avoid makeing queue too large.
+        }
+      return;
+    }
+
   struct pollfd inPollfd;
 
-  int fd = fileno(stdin);        // stdin file descriptor
   inPollfd.fd = fd;
   inPollfd.events = POLLIN;
 
@@ -152,21 +175,27 @@ Uart8250::monitorStdin()
 	    {
 	      std::lock_guard<std::mutex> lock(mutex_);
 	      char c;
-	      if (::read(fd, &c, sizeof(c)) != 1)
-		std::cerr << "Uart8250::monitorStdin: unexpected fail on read\n";
-	      if (isatty(fd))
-		{
-		  static char prev = 0;
+              auto rc = ::read(fd, &c, sizeof(c));
+              if (rc == 1)
+                {
+                  if (isatty(fd))
+                    {
+                      static char prev = 0;
 
-		  // Force a stop if control-a x is seen.
-		  if (prev == 1 and c == 'x')
-		    throw std::runtime_error("Keyboard stop");
-		  prev = c;
-		}
-	      rx_fifo.push(c);
-	      lsr_ |= 1;  // Set least sig bit of line status.
-	      iir_ &= ~1;  // Clear bit 0 indicating interrupt is pending.
-	      // setInterruptPending(true);
+                      // Force a stop if control-a x is seen.
+                      if (prev == 1 and c == 'x')
+                        throw std::runtime_error("Keyboard stop");
+                      prev = c;
+                    }
+                  rx_fifo.push(c);
+                  lsr_ |= 1;  // Set least sig bit of line status.
+                  iir_ &= ~1;  // Clear bit 0 indicating interrupt is pending.
+                  // setInterruptPending(true);
+                }
+              else if (rc == 0)
+                continue;
+              else
+		std::cerr << "Uart8250::monitorStdin: unexpected fail on read\n";
 	    }
 	}
 

Uart8250.hpp
@@ -4,6 +4,7 @@
 #include <atomic>
 #include <mutex>
 #include <queue>
+#include <termios.h>
 #include "IoDevice.hpp"
 
 
@@ -45,5 +46,7 @@ namespace WdRiscv
     std::atomic<bool> terminate_ = false;
     std::mutex mutex_;   // Synchronize access to byte_ with stdinThread_.
     std::queue<uint8_t> rx_fifo;
+
+    struct termios termAttr_;   // Terminal state.
   };
 }

Hart.cpp
@@ -1827,7 +1827,7 @@ readCharNonBlocking(int fd)
     return 0;
 
   char c = 0;
-  std::ptrdiff_t code = ::read(fd, &c, sizeof(c));
+  auto code = ::read(fd, &c, sizeof(c));
   if (code == 1)
     {
       if (isatty(fd))

commit 7bb907112ca4c30fda787f368bdc8ab89a7973b1

Fix Uart8250 bugs introduced by special treatment of TTYs
Commit 3e88a8b9cdaf496f68466c943fbde8256d8b65c4 introduced a number of
issues:
- By spinning when the FIFO is full, we eat CPU. We should be blocking
  using a condition variable or similar synchronization primitive.
- There are non-file, non-tty file descriptors which may not have all
  the input available when whisper starts. For example, one could pipe
  the output of a command into whisper. When these files return 0 on
  read, we'll stop trying to read from them and will never get data when
  it shows up later.

This is resolved by removing special treatment of TTYs. We handle all
file descriptors uniformly, but we read more than 1 byte at a time if
possible and limit how large the rx_fifo can get.

Uart8250.cpp
@@ -19,7 +19,7 @@ FDChannel::FDChannel(int in_fd, int out_fd)
   inPollfd.fd = in_fd_;
   inPollfd.events = POLLIN;
 
-  if (isTTY()) {
+  if (isatty(in_fd_)) {
     struct termios term;
     tcgetattr(in_fd_, &term);
     cfmakeraw(&term);
@@ -28,36 +28,37 @@ FDChannel::FDChannel(int in_fd, int out_fd)
   }
 }
 
-bool FDChannel::read(uint8_t& byte) {
-  if (not isTTY())
-    return ::read(in_fd_, &byte, 1) == 1;
-
-  int code = poll(&inPollfd, 1, 1);
+size_t FDChannel::read(uint8_t *arr, size_t n) {
+  int code = poll(&inPollfd, 1, -1);
 
   if (code == 0)
-    return false;
+    return 0;
 
   if (code == 1)
   {
     if ((inPollfd.revents & POLLIN) != 0)
     {
-      uint8_t c;
-      if (::read(in_fd_, &c, sizeof(c)) != 1)
-	std::cerr << "FDChannel: unexpected fail on read\n";
-      static uint8_t prev = 0;
+      ssize_t count = ::read(in_fd_, arr, n);
+      if (count < 0)
+	throw std::runtime_error("FDChannel: Failed to read from in_fd_\n");
 
-      // Force a stop if control-a x is seen.
-      if (prev == 1 and c == 'x')
-	throw std::runtime_error("Keyboard stop");
-      prev = c;
+      if (isatty(in_fd_))
+	for (size_t i = 0; i < static_cast<size_t>(count); i++) {
+	  static uint8_t prev = 0;
+	  const uint8_t c = arr[i];
 
-      byte = c;
-      return true;
+	  // Force a stop if control-a x is seen.
+	  if (prev == 1 and c == 'x')
+	    throw std::runtime_error("Keyboard stop");
+	  prev = c;
+	}
+
+      return count;
     }
   }
 
   // TODO: handle error return codes from poll.
-  return false;
+  return 0;
 }
 
 void FDChannel::write(uint8_t byte) {
@@ -68,10 +69,6 @@ void FDChannel::write(uint8_t byte) {
 
   if (written == -1)
     throw std::runtime_error("FDChannel error writing to output\n");
-}
-
-bool FDChannel::isTTY() {
-  return isatty(in_fd_);
 }
 
 PTYChannelBase::PTYChannelBase() {
@@ -101,13 +98,13 @@ Uart8250::Uart8250(uint64_t addr, uint64_t size,
   : IoDevice(addr, size, aplic, iid), channel_(std::move(channel))
 {
   auto func = [this]() { this->monitorInput(); };
-  stdinThread_ = std::thread(func);
+  inThread_ = std::thread(func);
 }
 
 Uart8250::~Uart8250()
 {
   terminate_ = true;
-  stdinThread_.join();
+  inThread_.join();
 }
 
 uint32_t
@@ -122,7 +119,7 @@ Uart8250::read(uint64_t addr)
 	{
 	case 0:
 	  {
-	    std::lock_guard<std::mutex> lock(mutex_);
+	    std::unique_lock<std::mutex> lock(mutex_);
 	    uint32_t res = 0;
 	    if (!rx_fifo.empty()) {
 	      res = rx_fifo.front();
@@ -133,6 +130,8 @@ Uart8250::read(uint64_t addr)
 	      iir_ |= 1;   // Set least sig bit indicating no interrupt.
 	      setInterruptPending(false);
 	    }
+	    lock.unlock();
+	    cv_.notify_all();
 	    return res;
 	  }
 
@@ -207,38 +206,36 @@ Uart8250::write(uint64_t addr, uint32_t value)
 }
 
 
+
 void
 Uart8250::monitorInput()
 {
-  if (!channel_->isTTY())
-    {
-      uint8_t c = 0;
-      while (channel_->read(c))
-      {
-	rx_fifo.push(c);
-
-	while (rx_fifo.size() > 1024)
-	  ;  // Avoid makeing queue too large.
-      }
-      return;
-    }
-
   while (true)
   {
     if (terminate_)
       return;
 
-    uint8_t c;
-    if (!channel_->read(c))
-      continue;
+    std::array<uint8_t, FIFO_SIZE> arr;
+    size_t count = channel_->read(arr.data(), FIFO_SIZE);
 
-    {
-      std::lock_guard<std::mutex> lock(mutex_);
-      rx_fifo.push(c);
 
-      lsr_ |= 1;  // Set least sig bit of line status.
-      iir_ &= ~1;  // Clear bit 0 indicating interrupt is pending.
-      setInterruptPending(true);
+    if (count != 0) {
+      std::unique_lock<std::mutex> lock(mutex_);
+
+      size_t i = 0;
+      do {
+	for (; i < count && rx_fifo.size() < FIFO_SIZE; i++)
+	{
+	  rx_fifo.push(arr[i]);
+	}
+
+	lsr_ |= 1;  // Set least sig bit of line status.
+	iir_ &= ~1;  // Clear bit 0 indicating interrupt is pending.
+	setInterruptPending(true);
+
+	// Block until rx_fifo has space
+	cv_.wait(lock, [this] () {return rx_fifo.size() < FIFO_SIZE;});
+      } while (i != count);
     }
   }
 }

Uart8250.hpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <condition_variable>
 #include <queue>
 #include <poll.h>
 #include "IoDevice.hpp"
@@ -19,22 +20,18 @@ namespace WdRiscv
     /// Block until a byte is available
     /// Return false on failure
     /// Return true on success and the read byte is placed in byte parameter
-    virtual bool read(uint8_t& byte) = 0;
+    virtual size_t read(uint8_t *buf, size_t size) = 0;
 
     /// Send the given byte
     virtual void write(uint8_t byte) = 0;
-
-    // Does this channel correspond to a TTY?
-    virtual bool isTTY() = 0;
   };
 
   class FDChannel : public UartChannel {
   public:
     FDChannel(int in_fd, int out_fd);
 
-    bool read(uint8_t& byte) override;
+    size_t read(uint8_t *buf, size_t size) override;
     void write(uint8_t byte) override;
-    bool isTTY() override;
 
   private:
     int in_fd_, out_fd_;
@@ -77,6 +74,8 @@ namespace WdRiscv
     void write(uint64_t addr, uint32_t value) override;
 
   private:
+    static const constexpr size_t FIFO_SIZE = 1024;
+
     std::unique_ptr<UartChannel> channel_;
 
     /// This runs in its own thread. It monitors the standard input and
@@ -96,9 +95,11 @@ namespace WdRiscv
     uint8_t dlm_ = 0x1;   // Divisor latch msb
     uint8_t psd_ = 0;     // Pre-scaler division
 
-    std::thread stdinThread_;
+    std::thread inThread_;
     std::atomic<bool> terminate_ = false;
-    std::mutex mutex_;   // Synchronize access to byte_ with stdinThread_.
+    std::mutex mutex_;   // Synchronize access to rx_fifo with inThread_.
+    std::condition_variable cv_; // Wake inThread_ when there is space in the rx_fifo
+
     std::queue<uint8_t> rx_fifo;
   };
 }