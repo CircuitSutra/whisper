d79ed1a7f1e6e5452722fe41902ff57da2bd2f28
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2025-03-03 09:00:53 -0600

    Zvqdot is renamed to Zvqdotq.

--- Isa.hpp
+++ Isa.hpp
@@ -15,7 +15,7 @@ namespace WdRiscv
                                       Zvfh, Zvfhmin, Zvbb, Zvbc, Zvkg,
 				      Zvkned, Zvknha, Zvknhb, Zvksed,
                                       Zvksh, Zvkb, Zicond, Zca, Zcb, Zcd, Zfa, Zfbfmin,
-                                      Zvfbfmin, Zvfbfwma, Zvqdot, Sstc, Svpbmt, Svadu, Svade,
+                                      Zvfbfmin, Zvfbfwma, Zvqdotq, Sstc, Svpbmt, Svadu, Svade,
 				      Smaia, Ssaia, Zacas, Zimop, Zcmop, Smrnmi,
 				      Zicsr, Zicntr, Zihpm, Zifencei, Zihintpause,
                                       Smmpm, Ssnpm, Smnpm, Sscofpmf, Smstateen,
