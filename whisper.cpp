a5fa2bf1527c23ab0309309fdf9beacad33e6524
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2025-06-25 14:43:40 -0500

    Revert "Updated README and whisper.cpp"
    This reverts commit aa76a6b909aa2c84d1e83e31baee747ac1a8ba21.

--- whisper.cpp
+++ whisper.cpp
@@ -94,7 +94,7 @@ main(int argc, char* argv[])
 	  Session<uint64_t> session{};
 	  ok = session.defineSystem(args, config) != nullptr;
 	  ok = ok and session.configureSystem(args, config);
-         ok = ok and session.run(args);
+    ok = ok and session.run(args);
           ok = ok and session.cleanup(args);
 	}
       else
@@ -109,6 +109,8 @@ main(int argc, char* argv[])
       std::cerr << e.what() << '\n';
       ok = false;
     }
+
+  std::cerr << "ALL done!\n";
 
   return ok? 0 : 1;
 }
